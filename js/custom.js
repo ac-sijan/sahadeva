489e70b9563694f208f84111d8134001970b149b
ac-sijan <65887700+ac-sijan@users.noreply.github.com>
2020-06-09 19:56:31 +0545

    first commit

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+
+/*===================================================
+               SMOOTH SCROLLING EFFECT
+=============================================*/
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get/return id like #about, #work, #team and etc
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    });
+});
+
+
+
+
+/*===================================================
+               BACK TO TOP
+=============================================*/
+
+var btn = $('#back2top-button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+
+
+// $('.navbar-nav>li>a').on('click', function(){
+//   $('.navbar-collapse').collapse('hide');
+// });
---
+++ js/pdf.js
@@ -0,0 +1,16 @@
+
+  var cvReq = document.querySelector('#cv');
+  cvReq.addEventListener("click",function(){
+    window.open(
+      "html/cv.html","_blank"
+    );
+  });
+
+  var travel = document.querySelector('#travel');
+  travel.addEventListener("click",function(){
+    location.replace(
+      "html/travel.html"
+    );
+  });
+
+
